cf077212bec15465ce21fb27cf9bbc6dad97b9d2
javohirwebdeveloper <jumaboyevjavohir07@gmail.com>
2023-11-29 20:31:32 +0500

    lesson-5-w1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error('Error fetching countries:', error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search countries..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <div>
+        {filteredCountries.map((country) => (
+          <div key={country.cca2} className="country-card">
+            <img
+              className="country-flag"
+              src={country.flags.svg}
+              alt={`${country.name.common} Flag`}
+            />
+            <div className="country-info">
+              <h2 className="country-name">{country.name.common}</h2>
+              <div className="country-details">
+                <p>Population: {country.population}</p>
+                <p>Region: {country.region}</p>
+                <p>Capital: {country.capital}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
